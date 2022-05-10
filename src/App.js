adf46a46afd5cbb390d8c53d120602d1366b51b4
unknown <thanhpot88@gmail.com>
2022-05-10 22:39:11 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useCallback, useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 function App() {
+  const [input, setInput] = useState("");
+  const [data, setData] = useState({});
+  const [shortLink, setShortLink] = useState("none");
+  const [check, setCheck] = useState(0);
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
+  };
+  const fetchApi = useCallback(async () => {
+    try {
+      const respone = await axios.get(
+        `https://api.shrtco.de/v2/shorten?url=${input}`
+      );
+      console.log(respone);
+      setData(respone.data.result);
+    } catch (err) {
+      console.log(err);
+    }
+  }, [input]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <form>
+        <span>Enter a Link :</span>
+        <input
+          placeholder="example.com"
+          value={input}
+          onChange={handleInputChange}
+        ></input>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            if (check === 1) setShortLink(data.short_link);
+            else if (check === 2) setShortLink(data.short_link2);
+            else if (check === 3) setShortLink(data.short_link3);
+            setInput("");
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Submit
+        </button>
+        <br></br>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            fetchApi();
+            setCheck(1);
+          }}
+        >
+          shrtco.de
+        </button>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            fetchApi();
+            setCheck(2);
+          }}
+        >
+          9qr.de
+        </button>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            fetchApi();
+            setCheck(3);
+          }}
+        >
+          shiny.link
+        </button>
+      </form>
+      <h3>{shortLink}</h3>
     </div>
   );
 }
